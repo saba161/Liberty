4046aee79ed911a502047b29317487e5a0ecd7a5
saba161 <saba.koguashvili1999@mail.ru>
2020-09-04 22:23:54 +0400

    s

---
+++ calculator/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ostrov2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> number = new Stack<int>();
+            Stack<char> operators = new Stack<char>();
+
+            var s = "1+2*(3+4/2-(1+2))*2+1".ToCharArray();
+
+            foreach(var item in s)
+            {
+                if(item == '+' || item == '-' || item == '/' || item == '*' || item == '(' || item == ')')
+                {
+                    if(operators.Count() != 0)
+                    {
+                        if(Priorities(operators.Peek()) < Priorities(item) || item == '(' || item == ')')
+                        {
+                            operators.Push(item);
+                        }
+                        else
+                        {
+                            int x = number.Peek();
+                            number.Pop();
+                            int y = number.Peek();
+                            number.Pop();
+                            int result = Operator(operators.Peek(), y, x);
+                            operators.Pop();
+                            number.Push(result);
+                            operators.Push(item);
+                        }
+                    }
+                    else
+                    {
+                        operators.Push(item);
+                    }
+                }
+                else
+                {
+                    number.Push(int.Parse(item.ToString()));
+                }
+            }
+        }
+        public static void Chek(char item)
+        {
+            
+        }
+
+        public static int Priorities(char oper)
+        {
+            switch(oper)
+            {
+                case '+':
+                    return 1;
+                case '-':
+                    return 1;
+                case '*':
+                    return 2;
+                case '/':
+                    return 2;
+            }
+
+            return 0;
+        }
+
+        public static int Operator(char oper, int x, int y)
+        {
+            switch(oper)
+            {
+                case '+':
+                    return x + y;
+                case '-':
+                    return x - y;
+                case '*':
+                    return x * y;
+                case '/':
+                    return x / y; 
+            }
+            throw new ArgumentException("Unexpected operator string: " + oper);
+        }
+    }
+}
---
+++ ostrov/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace ostrov
+{
+    class Program
+    {
+        public static int[,] matrix;
+        public static int columns;
+        public static int rows;
+
+        static void Main(string[] args)
+        {
+            columns = int.Parse(Console.ReadLine());
+            rows = int.Parse(Console.ReadLine());
+
+            int count = 0;
+            matrix = new int[rows, columns];
+
+            FillTheMatrix(rows, columns);
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (matrix[i, j] == 1)
+                    {
+                        count++;
+                        Check(i, j);
+                    }
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+
+        public static bool Check(int row, int col)
+        {
+            if (row < 0 || col < 0 || col >= columns || row >= rows)
+            {
+                return false;
+            }
+
+            if (matrix[row, col] != 1)
+            {
+                return false;
+            }
+
+            matrix[row, col] = 2;
+
+            Check(row + 1, col);
+            Check(row - 1, col);
+            Check(row, col + 1);
+            Check(row, col - 1); 
+
+            return true;
+        }
+
+        public static void FillTheMatrix(int row, int col)
+        {
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    matrix[i,j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+        }
+    }
+}
